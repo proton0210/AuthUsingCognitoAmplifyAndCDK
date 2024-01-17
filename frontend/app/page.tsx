16885a471b1893c7dfa0fa7a21591603cabde2e2
Vidit Shah <vidit0210@protonmail.com>
2024-01-17 20:41:22 +0530

    Fixed the username Issue

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,22 +1,10 @@
-"use client";
+import Greet from "@/components/greet";
 
-import { getCurrentUser } from "aws-amplify/auth";
-
-export async function getUserName() {
-  try {
-    const { username, userId, signInDetails } = await getCurrentUser();
-    return username;
-  } catch (err) {
-    console.log(err);
-  }
-}
-async function Page() {
-  const username = await getUserName();
-
+function Page() {
   return (
     <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-xl lg:mt-20">
-        <h1>Hello {username}</h1>
+        <Greet />
       </div>
     </div>
   );
